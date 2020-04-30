96fe57a83a1247f0af5e239e02543be722e53065
vincent73772 <vincentkurniawan737@gmail.com>
2020-04-30 19:41:08 +0700

    berhasil menhghapus data service

--- controller.js
+++ controller.js
@@ -276,7 +276,7 @@ exports.ubahlevel = function (req, res) {
         });
 };
 
-//Mengubah data User
+//Mengubah data service
 exports.ubahservice = function (req, res) {
     var id_service = req.body.id_service;
     var tgl_service = new Date();
@@ -295,4 +295,17 @@ exports.ubahservice = function (req, res) {
                 response.ok("Berhasil Mengubah Data", res)
             }
         });
+};
+
+//menghapus data montir
+exports.hapusMontir = function(req, res){
+    var id = req.body.id_montir;
+    connection.query('DELETE FROM t_montir WHERE id_montir=?', [id],
+    function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok("Berhasil Hapus Data", res)
+        }
+    });
 };
--- routes.js
+++ routes.js
@@ -54,5 +54,8 @@ module.exports = function(app){
     app.route('/ubahservice')
     .put(jsonku.ubahservice);
 
+    app.route('/hapusmontir')
+    .delete(jsonku.hapusMontir);
+
 }
    
